bf0d11a2cfa04179e36be5a5373cecfa93eb3d21
Mert Özkaymak <101987240+mertozkaymak@users.noreply.github.com>
2022-03-21 02:49:48 +0300

    Update api.config.php

--- ideasoft/api.config.php
+++ ideasoft/api.config.php
@@ -2,15 +2,15 @@
 
     //Database Settings
     define("HOST", "localhost");
-    define("DBNAME", "delta_form");
-    define("USER", "delta_form");
-    define("PASS", "Digital1357*c");
+    define("DBNAME", "***");
+    define("USER", "***");
+    define("PASS", "***");
 
     //Shop Settings
-    define("URL", "https://deltakalip.myideasoft.com");
-    define("CLIENTID", "10_1mm4e0p1hvtwwgsccsssw0cgkkwg8owcwck0kcsgoocsw8kc4w");
-    define("SECRETID", "1nlacctof3eskg8goo0884os00wcgc0c8wkk8g40kkkksw40cw");
-    define("REDIRECT", "https://dev.digitalfikirler.com/delta_form/controller.php");
-    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=d12b2a61b81a52be4a4cac7e610f62f8&redirect_uri=" . REDIRECT);
+    define("URL", "***");
+    define("CLIENTID", "***");
+    define("SECRETID", "***");
+    define("REDIRECT", "***");
+    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=***&redirect_uri=" . REDIRECT);
 
 ?>
