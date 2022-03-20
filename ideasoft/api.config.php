24a6ee56e3e7b2ca3c649ee33e7211faacf28487
Mert Özkaymak <101987240+mertozkaymak@users.noreply.github.com>
2022-03-20 23:06:41 +0300

    Add files via upload

---
+++ ideasoft/api.config.php
@@ -0,0 +1,16 @@
+<?php
+
+    //Database Settings
+    define("HOST", "localhost");
+    define("DBNAME", "delta_form");
+    define("USER", "delta_form");
+    define("PASS", "Digital1357*c");
+
+    //Shop Settings
+    define("URL", "https://deltakalip.myideasoft.com");
+    define("CLIENTID", "10_1mm4e0p1hvtwwgsccsssw0cgkkwg8owcwck0kcsgoocsw8kc4w");
+    define("SECRETID", "1nlacctof3eskg8goo0884os00wcgc0c8wkk8g40kkkksw40cw");
+    define("REDIRECT", "https://dev.digitalfikirler.com/delta_form/controller.php");
+    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=d12b2a61b81a52be4a4cac7e610f62f8&redirect_uri=" . REDIRECT);
+
+?>
